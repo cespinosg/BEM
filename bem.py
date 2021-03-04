005e0792180dcf02c716d60874bbd0f4773a9e0d
NikPerujo <npperujo@gmail.com>
2021-03-04 21:03:09 +0100

    added plot function

--- bem.py
+++ bem.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class Blade():
@@ -52,7 +53,7 @@ class BEMSolver():
         '''
         Calculates the Prandtl loss factor.
         '''
-        d = 2*np.pi/self.blade.n_blades*(1-a)/np.sqrt(self.tsr**2+(1-a)**2)
+        d = 2*np.pi/self.blade.n_blades*(1-a)/np.sqrt(self.tsr**2+1/(mu**2)*(1-a)**2)
         f_tip = 2/np.pi*np.arccos(np.exp(-np.pi*(1-mu)/d))
         mu_hub = self.blade.df['mu'][0]
         f_root = 2/np.pi*np.arccos(np.exp(-np.pi*(mu-mu_hub)/d))
@@ -73,6 +74,7 @@ class BEMSolver():
         chord, twist = self.blade.get_geo(mu)
         sigma_r = self.blade.n_blades*chord/(2*np.pi*mu*self.blade.radius)
         a0, ap0 = 1/3, 0
+        fx, fy = 0, 0
         error = tol+1
         i = 0
         while tol < error:
@@ -89,11 +91,58 @@ class BEMSolver():
             # Relaxing the induction factors is necessary for convergence near
             # the root
             a0, ap0 = 0.75*a0+0.25*a, 0.75*ap0+0.25*ap
-            print(i)
+            fx = 0.5*(w2_u2*100)*cx*chord #Uo2 = 100
+            fy = 0.5*(w2_u2*100)*cy*chord #Uo2 = 100
+            #print(i)
             i += 1
-        return a, ap
+        return a, ap, fx, fy
+
+def graph(blade, solver):
+    Uo = 10 #m/s
+    omega = Uo*solver.tsr/blade.radius
+    dr = 0.01
+    mu = np.arange(0.2+dr/2, 1.0-dr/2, dr) #midpoints of annuli
+    print(len(mu))
+    a, ap, fx, fy = np.zeros(len(mu)), np.zeros(len(mu)), np.zeros(len(mu)), np.zeros(len(mu))
+
+    for i in range(len(mu)):
+        a[i], ap[i], fx[i], fy[i] = solver.solve(mu[i])
+        print(i)
+
+    CT = np.sum(dr*fx*blade.n_blades/(0.5*Uo**2*np.pi*blade.radius**2))
+    CP = np.sum(dr*fy*mu*blade.n_blades*blade.radius*omega/(0.5*Uo**3*np.pi*blade.radius**2))
+    print("CT = ", CT)
+    print("CP = ", CP)
+
+    '''
+    plot a and a'
+    '''
+    fig1 = plt.figure(figsize=(12, 6))
+    plt.title('Axial and tangential induction')
+    plt.plot(mu, a, 'b-', label=r'$a$')
+    plt.plot(mu, ap, 'g--', label=r'$a^,$')
+    plt.grid()
+    plt.xlabel('r/R')
+    plt.legend()
+    plt.savefig("a_aip.png")
+    plt.show()
+
+    '''
+    plot Fax and Faz
+    '''
+    fig1 = plt.figure(figsize=(12, 6))
+    plt.title(r'Normal and tagential force, non-dimensioned by $\frac{1}{2} \rho U_\infty^2 R$')
+    plt.plot(mu, fx/(0.5*Uo**2*blade.radius), 'b-', label=r'Faxial')
+    plt.plot(mu, fy/(0.5*Uo**2*blade.radius), 'g--', label=r'Fazimuthal')
+    plt.grid()
+    plt.xlabel('μ')
+    plt.legend()
+    plt.savefig("Fax_Faz.png") 
+    plt.show()
 
 if __name__ == '__main__':
     blade = Blade()
     solver = BEMSolver(blade, 6)
-    a, ap = solver.solve(0.505)
+    #a, ap, fx, fy = solver.solve(0.505)
+    #print(a, ap, fx, fy)
+    graph(blade, solver)
