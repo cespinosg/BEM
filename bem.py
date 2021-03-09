505554f09504ae5a8cf6a0c47e4570157f31d438
NikPerujo <npperujo@gmail.com>
2021-03-09 18:29:48 +0100

    tentatively added Glauert correction

--- bem.py
+++ bem.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from graph import *
 
 
 class Blade():
@@ -67,7 +67,19 @@ class BEMSolver():
         phi = np.arctan2(1-a, self.tsr*mu*(1+ap))
         return phi
 
-    def solve(self, mu, tol=1e-9):
+    def get_a(self, CT):
+        '''
+        Calculates a given CT applies glauert correction.
+        '''
+        CT1 = 1.816
+        CT2 = 2*np.sqrt(CT1) - CT1
+        if(CT < CT2):
+            a = 0.5 - 0.5*np.sqrt(1-CT)
+        elif(CT >= CT2):
+            a = 1 + (CT-CT1)/(4*np.sqrt(CT1)-4)
+        return a
+
+    def solve(self, mu, dmu, tol=1e-9):
         '''
         Solves the BEM equations at the given section.
         '''
@@ -75,6 +87,7 @@ class BEMSolver():
         sigma_r = self.blade.n_blades*chord/(2*np.pi*mu*self.blade.radius)
         a0, ap0 = 1/3, 0
         fx, fy = 0, 0
+        area = np.pi*(((mu+0.5*dmu)*self.blade.radius)**2 - ((mu-0.5*dmu)*self.blade.radius)**2)
         error = tol+1
         i = 0
         while tol < error:
@@ -84,61 +97,24 @@ class BEMSolver():
             w2_u2 = (1-a0)**2+(self.tsr*mu*(1+ap0))**2
             cx = cl*np.cos(phi)+cd*np.sin(phi)
             cy = cl*np.sin(phi)-cd*np.cos(phi)
+            fx = 0.5*(w2_u2*100)*cx*chord #Uo2 = 100
+            fy = 0.5*(w2_u2*100)*cy*chord #Uo2 = 100
+            CT = fx*self.blade.n_blades*dmu*self.blade.radius/(0.5*100*area)
             f = self.get_prandtl(a0, mu)
-            a = w2_u2*sigma_r*cx/(4*(1-a0)*f)
-            ap = w2_u2*sigma_r*cy/(4*(1-a0)*self.tsr*mu*f)
+            a = self.get_a(CT)
+            ap = fy*self.blade.n_blades/(4*np.pi*mu*self.blade.radius*100*(1-a)*self.tsr*mu)
+            a = a/f
+            ap = ap/f
+            #a = w2_u2*sigma_r*cx/(4*(1-a0)*f)
+            #ap = w2_u2*sigma_r*cy/(4*(1-a0)*self.tsr*mu*f)
             error = max([abs(a-a0), abs(ap-ap0)])
             # Relaxing the induction factors is necessary for convergence near
             # the root
             a0, ap0 = 0.75*a0+0.25*a, 0.75*ap0+0.25*ap
-            fx = 0.5*(w2_u2*100)*cx*chord #Uo2 = 100
-            fy = 0.5*(w2_u2*100)*cy*chord #Uo2 = 100
             #print(i)
             i += 1
         return a, ap, fx, fy
 
-def graph(blade, solver):
-    Uo = 10 #m/s
-    omega = Uo*solver.tsr/blade.radius
-    dr = 0.01
-    mu = np.arange(0.2+dr/2, 1.0-dr/2, dr) #midpoints of annuli
-    print(len(mu))
-    a, ap, fx, fy = np.zeros(len(mu)), np.zeros(len(mu)), np.zeros(len(mu)), np.zeros(len(mu))
-
-    for i in range(len(mu)):
-        a[i], ap[i], fx[i], fy[i] = solver.solve(mu[i])
-        print(i)
-
-    CT = np.sum(dr*fx*blade.n_blades/(0.5*Uo**2*np.pi*blade.radius**2))
-    CP = np.sum(dr*fy*mu*blade.n_blades*blade.radius*omega/(0.5*Uo**3*np.pi*blade.radius**2))
-    print("CT = ", CT)
-    print("CP = ", CP)
-
-    '''
-    plot a and a'
-    '''
-    fig1 = plt.figure(figsize=(12, 6))
-    plt.title('Axial and tangential induction')
-    plt.plot(mu, a, 'b-', label=r'$a$')
-    plt.plot(mu, ap, 'g--', label=r'$a^,$')
-    plt.grid()
-    plt.xlabel('r/R')
-    plt.legend()
-    plt.savefig("a_aip.png")
-    plt.show()
-
-    '''
-    plot Fax and Faz
-    '''
-    fig1 = plt.figure(figsize=(12, 6))
-    plt.title(r'Normal and tagential force, non-dimensioned by $\frac{1}{2} \rho U_\infty^2 R$')
-    plt.plot(mu, fx/(0.5*Uo**2*blade.radius), 'b-', label=r'Faxial')
-    plt.plot(mu, fy/(0.5*Uo**2*blade.radius), 'g--', label=r'Fazimuthal')
-    plt.grid()
-    plt.xlabel('μ')
-    plt.legend()
-    plt.savefig("Fax_Faz.png") 
-    plt.show()
 
 if __name__ == '__main__':
     blade = Blade()
